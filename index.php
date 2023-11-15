b0ad17c6fd5ca50e701aed56e376a80e62bbd8a8
Ali <alicikampek21@gmail.com>
2023-11-15 08:39:17 +0700

    second commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+
+$server_key = "SB-Mid-server-PsLktmPKSrW44UEbRvvO_jaB";
+$is_production = false;
+
+$api_url = $is_production;
+
+if(!strpos($_SERVER["REQUEST_URL"],'charge')){
+    http_response_code(404);
+    echo "wrong path, make sure it's '/change'";exit();
+}
+
+if($_SERVER['REQUEST_METHOD']!=='POST'){
+    http_response_code(404);
+    echo 'Page not found or wrong HTTP request method is used';
+}
+
+$request_body = file_get_contents('php://input');
+header('Content-Type: application/json');
+
+$charge_result = chargeApi($api_url,$server_key,$request_body);
+http_response_code($charge_result['http_code']);
+
+echo $charge_result['body'];
+
+function chargeApi($api_url,$server_key,$request_body){
+    $ch = curl_init();
+    $curl_option = array(
+        CURLOPT_URL => $api_url,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+
+        CURLOPT_HTTPHEADER => array(
+            'Content_Type : application/json',
+            'Accept: application/json',
+            'Authorization: Basic'. base64_decode($server_key)
+        ),
+    );
+
+    curl_setopt_array($ch,$curl_option);
+    $result = array(
+        'body' => curl_exec($ch),
+        'http_code' => curl_getinfo($sc,CURLINFO_HTTP_CODE)
+    );
+
+    return $result;
+}
